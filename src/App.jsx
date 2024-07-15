12235667cafdddd1384242bc72b51fcc7ec24423
Davide Giliberti <gilo.davide@hotmail.it>
2024-07-15 18:18:23 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import MainProfile from "./components/MainProfile";
+import AsideProfile from "./components/AsideProfile";
 
 function App() {
-  return <MainProfile />;
+  return (
+    <>
+      <MainProfile />
+      <AsideProfile />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/AsideProfile.jsx
@@ -0,0 +1,69 @@
+import { Button, Card, Col } from "react-bootstrap";
+import { Pencil } from "react-bootstrap-icons";
+
+const AsideProfile = () => (
+  <>
+    <Col sm={2}>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            Lingua del profilo <Pencil />
+          </Card.Title>
+          <Card.Text>Italiano</Card.Text>
+        </Card.Body>
+        <hr />
+        <Card.Body>
+          <Card.Title>
+            Profilo pubblico e URL <Pencil />
+          </Card.Title>
+          <Card.Text>www.linkedin.com/in/profile</Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Card className="mt-2">
+        <h6 className="p-3">Altri profili simili</h6>
+        <Card.Body>
+          <Card.Img
+            variant="top"
+            src="https://c8.alamy.com/compit/2f8dh42/nessuna-foto-o-icona-immagine-vuota-caricamento-di-immagini-o-contrassegno-immagine-mancante-immagine-non-disponibile-o-immagine-in-arrivo-segno-silhouette-naturale-semplice-nella-cornice-2f8dh42.jpg"
+            className="rounded-circle "
+            style={{ width: "50px", height: "50px" }}
+          />
+          <Card.Title>
+            Nome e cognome · <span className="text-secondary"> 2°</span>
+          </Card.Title>
+          <Card.Text>Lavora per...</Card.Text>
+          <Button
+            size="sm"
+            variant="outline-secondary"
+            className="rounded-pill"
+          >
+            Collegati
+          </Button>
+        </Card.Body>
+        <hr />
+        <Card.Body>
+          <Card.Img
+            variant="top"
+            src="https://c8.alamy.com/compit/2f8dh42/nessuna-foto-o-icona-immagine-vuota-caricamento-di-immagini-o-contrassegno-immagine-mancante-immagine-non-disponibile-o-immagine-in-arrivo-segno-silhouette-naturale-semplice-nella-cornice-2f8dh42.jpg"
+            className="rounded-circle "
+            style={{ width: "50px", height: "50px" }}
+          />
+          <Card.Title>
+            Nome e cognome · <span className="text-secondary"> 2°</span>
+          </Card.Title>
+          <Card.Text>Lavora per...</Card.Text>
+          <Button
+            size="sm"
+            variant="outline-secondary"
+            className="rounded-pill"
+          >
+            Collegati
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  </>
+);
+
+export default AsideProfile;
