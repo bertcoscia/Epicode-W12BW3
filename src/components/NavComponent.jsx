db7cfbb058368a9677b6b3f4b92ea83a96d666c0
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-15 16:24:45 +0200

    add responsiveness to navbar

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,58 +1,72 @@
 import { Form, InputGroup, NavDropdown } from "react-bootstrap";
+import { Search, ThreeDots } from "react-bootstrap-icons";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 function NavComponent() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" style={{ height: "82px" }}>
+    <Navbar className="bg-body-tertiary" style={{ height: "80px" }}>
       <Container>
         <Navbar.Brand href="#home">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="#0966C2" width="45" height="45" focusable="false">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="#0966C2" width="45" height="45">
             <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
           </svg>
         </Navbar.Brand>
-
-        <Form inline>
+        <Form inline className="nav-search">
           <InputGroup>
-            <Form.Control type="text" placeholder="Search" style={{ backgroundColor: "#EDF3F8" }} className="px-4 py-2" />
+            <InputGroup.Text id="search-icon" className="search-input border-end-none">
+              <Search />
+            </InputGroup.Text>
+            <Form.Control type="text" placeholder="Search" className="px-2 py-2 search-input border-start-none nav-search-bar" aria-label="search" aria-describedby="search-icon" />
           </InputGroup>
         </Form>
 
-        <Nav className="mx-auto text-center d-flex align-items-center">
+        <Nav className="mx-md-auto text-center d-flex align-items-center">
+          <Nav.Link href="#home" className="d-flex flex-column align-items-center nav-search-icon">
+            <Search width={24} height={24} />
+            <small className="d-none d-lg-block">Search</small>
+          </Nav.Link>
           <Nav.Link href="#home" className="d-flex flex-column align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24">
               <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
             </svg>
-            Home
+            <small className="d-none d-lg-block">Home</small>
           </Nav.Link>
           <Nav.Link href="#home" className="d-flex flex-column align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24">
               <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
             </svg>
-            My Network
+            <small className="d-none d-lg-block">My Network</small>
           </Nav.Link>
           <Nav.Link href="#home" className="d-flex flex-column align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24">
               <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
             </svg>
-            Jobs
+            <small className="d-none d-lg-block">Jobs</small>
           </Nav.Link>
           <Nav.Link href="#home" className="d-flex flex-column align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24">
               <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
             </svg>
-            Messaging
+            <small className="d-none d-lg-block">Messaging</small>
           </Nav.Link>
           <Nav.Link href="#home" className="d-flex flex-column align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24">
               <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
             </svg>
-            Notifications
+            <small className="d-none d-lg-block">Notifications</small>
           </Nav.Link>
-          <Nav.Link href="#home" className="d-flex flex-column align-items-center border-end border-1">
-            <img src="https://thispersondoesnotexist.com/" alt="profile pic" className="rounded-circle" style={{ width: "24px", height: "24px", objectFit: "contain" }} />
-            <NavDropdown id="nav-dropdown-dark-example" title="Me" menuVariant="">
+          <Nav.Link href="#home" className="border-end border-1 d-none d-sm-block">
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title={
+                <div className="d-flex flex-column align-items-center">
+                  <img src={"https://thispersondoesnotexist.com/"} alt="profile pic" className="rounded-circle mb-1" style={{ width: "24px", height: "24px", objectFit: "contain" }} />
+                  <small className="d-none d-lg-block">Me</small>
+                </div>
+              }
+            >
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
@@ -60,16 +74,61 @@ function NavComponent() {
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav.Link>
-          <Nav.Link href="#home" className="d-flex flex-column align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24">
-              <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
-            </svg>
-            <NavDropdown id="nav-dropdown-dark-example" title="For business" menuVariant="">
+          <Nav.Link href="#home" className="d-none d-sm-block">
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title={
+                <div className="d-flex flex-column align-items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24">
+                    <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
+                  </svg>
+                  <small className="d-none d-lg-block">For Business</small>
+                </div>
+              }
+            >
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav.Link>
+          <Nav.Link href="#home" className="d-sm-none">
+            <NavDropdown id="nav-dropdown-dark-example" title={<ThreeDots fill="currentColor" width="30" height="30" />} drop="start">
+              <NavDropdown
+                id="nav-dropdown-dark-example"
+                title={
+                  <div className="d-flex flex-column align-items-center">
+                    <img src={"https://thispersondoesnotexist.com/"} alt="profile pic" className="rounded-circle mb-1" style={{ width: "24px", height: "24px", objectFit: "contain" }} />
+                    <small className="d-none d-lg-block">Me</small>
+                  </div>
+                }
+                drop="down-centered"
+              >
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+
+              <NavDropdown
+                id="nav-dropdown-dark-example"
+                title={
+                  <div className="d-flex flex-column align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24">
+                      <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
+                    </svg>
+                    <small className="d-none d-lg-block">For Business</small>
+                  </div>
+                }
+              >
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
             </NavDropdown>
           </Nav.Link>
         </Nav>
@@ -79,12 +138,3 @@ function NavComponent() {
 }
 
 export default NavComponent;
-
-{
-  /* SEARCH
-      <svg role="none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" data-supported-dps="24x24" data-test-icon="search-medium">
-        <use href="#search-medium" width="24" height="24"></use>
-      </svg>
-
-      */
-}
