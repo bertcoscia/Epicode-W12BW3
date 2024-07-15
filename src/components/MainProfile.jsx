d7546c252aa2530eae0dc1619021ce5e9e6c2780
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-07-15 16:05:54 +0200

    card Experience

--- src/components/MainProfile.jsx
+++ src/components/MainProfile.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Card, Button, Image } from "react-bootstrap";
-
+import { Card, Button, Image, CardHeader, CardBody } from "react-bootstrap";
+import { Pencil } from "react-bootstrap-icons";
 function MainProfile() {
   return (
     <>
-      <Card className=" mb-2 ps-2">
+      <Card id="profile" className=" mb-2 ps-2">
         <Card.Header className="p-0">
           <Card.Img
             style={{ height: "200px" }}
@@ -50,11 +50,14 @@ function MainProfile() {
         </Card.Body>
       </Card>
 
-      <Card className="bg-white py-4 px-3 rounded mb-2">
+      <Card id="about" className="bg-white py-4 px-3 rounded mb-2">
         <Card.Body>
           <div className="d-flex justify-content-between">
-            <Card.Title>About</Card.Title>
-            <div>icon</div>
+            <Card.Title>
+              <h1 className="fs-3">About</h1>
+            </Card.Title>
+            <div className="d-flex align-items-center"></div>
+            <Pencil />
           </div>
           <Card.Text>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, cum
@@ -65,15 +68,19 @@ function MainProfile() {
         </Card.Body>
       </Card>
 
-      <Card className="bg-white pt-4 px-3 rounded mb-2">
+      <Card id="activity" className="bg-white pt-4 px-3 rounded mb-2">
         <Card.Body>
           <div className="d-flex justify-content-between">
-            <Card.Title>Activity</Card.Title>
+            <Card.Title>
+              <h1 className="fs-3">Activity</h1>
+            </Card.Title>
             <div className="d-flex">
               <Button variant="outline-primary" className="rounded-pill">
                 Primary
               </Button>
-              <div>icon</div>
+              <div className="d-flex align-items-center ps-2">
+                <Pencil />
+              </div>
             </div>
           </div>
           <div>
@@ -102,31 +109,53 @@ function MainProfile() {
           <div className="fs-5 fw-bold text-center py-2">Show All Posts</div>
         </Card.Body>
       </Card>
+      <Card id="experience" className="bg-white pt-4 px-3 rounded mb-2">
+        <h1 className="fs-3 ps-2">Experience</h1>
+        <CardBody className="d-flex bg-white border-0 ps-2 pb-2 border border-bottom">
+          <div>
+            <img
+              width={"50px"}
+              src="https://c8.alamy.com/compit/2f8dh42/nessuna-foto-o-icona-immagine-vuota-caricamento-di-immagini-o-contrassegno-immagine-mancante-immagine-non-disponibile-o-immagine-in-arrivo-segno-silhouette-naturale-semplice-nella-cornice-2f8dh42.jpg"
+              alt="pic profile agency"
+            />
+          </div>
+          <div className="ms-2">
+            <h1 className="fs-4 mb-0">Ruolo</h1>
+            <p className="mb-0">Agenzia</p>
+            <p className="mb-0">Apr 2023 - Sept 2023 &bull; 6 mos</p>
+            <p>Luogo</p>
+            <div className="pt-0">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              tempore vel necessitatibus. A voluptate iure quo placeat
+              repellendus molestiae quas quod, enim voluptatum velit, reiciendis
+              cum aut voluptates. Aliquid, voluptatibus?
+            </div>
+          </div>
+        </CardBody>
+        <CardBody className="d-flex bg-white border-0 ps-2 pb-0">
+          <div>
+            <img
+              width={"50px"}
+              src="https://c8.alamy.com/compit/2f8dh42/nessuna-foto-o-icona-immagine-vuota-caricamento-di-immagini-o-contrassegno-immagine-mancante-immagine-non-disponibile-o-immagine-in-arrivo-segno-silhouette-naturale-semplice-nella-cornice-2f8dh42.jpg"
+              alt="pic profile agency"
+            />
+          </div>
+          <div className="ms-2">
+            <h1 className="fs-4 mb-0">Ruolo</h1>
+            <p className="mb-0">Agenzia</p>
+            <p className="mb-0">Apr 2023 - Sept 2023 &bull; 6 mos</p>
+            <p>Luogo</p>
+            <div className="pt-0">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              tempore vel necessitatibus. A voluptate iure quo placeat
+              repellendus molestiae quas quod, enim voluptatum velit, reiciendis
+              cum aut voluptates. Aliquid, voluptatibus?
+            </div>
+          </div>
+        </CardBody>
+      </Card>
     </>
   );
 }
 
 export default MainProfile;
-
-/* import { Col, Row } from "react-bootstrap";
-
-function HomepageCard() {
-  return (
-    <Row>
-      <Col>
-        <div className="bg-white py-4 px-3 rounded">
-          <div className="d-flex justify-content-between">
-            <h1>About</h1> <div>icons</div>
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, cum
-            modi aspernatur impedit perspiciatis nam rem laudantium a ut.
-            Voluptas molestiae ea soluta ad exercitationem eligendi, culpa
-            ducimus saepe pariatur.
-          </div>
-        </div>
-      </Col>
-    </Row>
-  );
-}
-export default HomepageCard; */
