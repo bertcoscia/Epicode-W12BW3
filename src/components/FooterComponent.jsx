d81431545c57ec335b13db68db9f6dd48b9d33a7
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-07-16 15:49:57 +0200

    fixed footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,3 +1,4 @@
+import { Dropdown, DropdownButton } from "react-bootstrap";
 import {
   CaretDownFill,
   GearFill,
@@ -5,11 +6,11 @@ import {
   ShieldShaded,
 } from "react-bootstrap-icons";
 
-function Footer() {
+function FooterComponent() {
   return (
-    <section className="container">
-      <div className="row">
-        <div className="col-12 col-md-3 mb-4">
+    <section id="footer" className="container">
+      <div className="row mt-5">
+        <div className="col-12 col-md-2 mb-4 me-3">
           <p>About</p>
           <p>Community Guidelines</p>
           <p>
@@ -18,25 +19,25 @@ function Footer() {
           <p>Sales Solutions</p>
           <p>Safety Center</p>
         </div>
-        <div className="col-12 col-md-3 mb-4">
+        <div className="col-12 col-md-2 mb-4 me-3">
           <p>Acessibility</p>
           <p>Careers</p>
           <p>Ad Choices</p>
           <p>Mobile</p>
         </div>
-        <div className="col-12 col-md-3 mb-4">
+        <div className="col-12 col-md-2 mb-4 me-3">
           <p>Talent Solutions</p>
           <p>Marketing Solutions</p>
           <p>Advertising</p>
           <p>Small Business</p>
         </div>
-        <div className="col-12 col-md-3">
+        <div className="col-12 col-md-2 me-3">
           <div className="d-flex mb-3">
             <div className="me-2">
               <QuestionCircleFill />
             </div>
             <div>
-              <h5 className="fs-5 mb-0">Questions?</h5>
+              <h5 className="fs-6 mb-0">Questions?</h5>
               <p>Visit our Help Center</p>
             </div>
           </div>
@@ -45,7 +46,7 @@ function Footer() {
               <GearFill />
             </div>
             <div>
-              <h5 className="fs-5 mb-0">Manage your account and privacy</h5>
+              <h5 className="fs-6 mb-0">Manage your account and privacy</h5>
               <p>Go to your Settings</p>
             </div>
           </div>
@@ -54,14 +55,33 @@ function Footer() {
               <ShieldShaded />
             </div>
             <div>
-              <h5 className="fs-5 mb-0">Recommendation transparency</h5>
+              <h5 className="fs-6 mb-0">Recommendation transparency</h5>
               <p>Learn more about Recommended Content.</p>
             </div>
           </div>
+        </div>
+        <div className="col-12 col-md-2">
+          <p className="m-0">Select Language</p>
+          <DropdownButton
+            variant={"outline-secondary"}
+            title={
+              <span>
+                English <CaretDownFill className="iconFooter" />
+              </span>
+            }
+          >
+            <Dropdown.Item eventKey="1">Italian</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3" active>
+              Active Item
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </DropdownButton>
         </div>
       </div>
     </section>
   );
 }
 
-export default Footer;
+export default FooterComponent;
